802a451686a82edabff9cffd085f33184306b1ff
SaurabhGope <saurabhgope93@gmail.com>
2025-03-08 19:16:27 +0530

    added glad,glfw and glm for openGL

--- main.cpp
+++ main.cpp
@@ -1,45 +1,94 @@
 #include <iostream>
 #include "grid.h"
 #include "ansi_escapes.h"
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+#include <glm/glm.hpp>
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+{
+    glViewport(0, 0, width, height);
+}
 
 int main(int argc, char* argv[])
 {
-	std::cout << "Conway's Game of Life \n";
-	std::cout << "Press the return key to display each generation\n";
-
-	// Wait for user
-	std::cin.get();
-
-	// Enable ANSI escape codes on Windows
-	SetupConsole();
-
-	// Grid for the first generation
-	Grid currentGeneration;
-
-	// Populate the cells at random
-	currentGeneration.Randomize();
-
-	while (true)
+	// Initialize GLFW
+	if (!glfwInit())
 	{
-		// Draw the current generation
-		currentGeneration.Draw();
+		std::cerr << "Failed to initialize GLFW!" << std::endl;
+		return -1;
+	}
+	//Printing the version of GLFW
+	std::cout << "GLFW Initialized! Version: " << glfwGetVersionString() << std::endl;
 
-		// Wait for user to press the return key
-		std::cin.get();
+	GLFWwindow* window = glfwCreateWindow(800, 600, "Conway's Game of Life", nullptr, nullptr);
+    if (!window)
+	{
+        std::cerr << "Failed to create GLFW window!" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+    glfwMakeContextCurrent(window);
+	
+	// Load GLAD (must be done after creating OpenGL context)
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+	{
+		std::cerr << "Failed to initialize GLAD!" << std::endl;
+		return -1;
+	}
+	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+	
+	// GLM Test
+    glm::vec3 position(1.0f, 2.0f, 3.0f);
+    std::cout << "GLM Vector: (" << position.x << ", " << position.y << ", " << position.z << ")" << std::endl;
+	// Main render loop
+	while (!glfwWindowShouldClose(window))
+	{
+		glClear(GL_COLOR_BUFFER_BIT);
 
-		// Grid for the next generation
-		Grid nextGeneration;
-
-		// Populate the cells in the next generation
-		Calculate(currentGeneration, nextGeneration);
-
-		// Update to the next generation
-		currentGeneration.Update(nextGeneration);
+		glfwSwapBuffers(window);
+		glfwPollEvents();
 	}
 
-	// Move cursor to bottom of screen
-	std::cout << "\x1b[" << 0 << ";" << ROW_MAX - 1 << "H";
+	// std::cout << "Conway's Game of Life \n";
+	// std::cout << "Press the return key to display each generation\n";
 
+	// // Wait for user
+	// std::cin.get();
+
+	// // Enable ANSI escape codes on Windows
+	// SetupConsole();
+
+	// // Grid for the first generation
+	// Grid currentGeneration;
+
+	// // Populate the cells at random
+	// currentGeneration.Randomize();
+
+	// while (true)
+	// {
+	// 	// Draw the current generation
+	// 	currentGeneration.Draw();
+
+	// 	// Wait for user to press the return key
+	// 	std::cin.get();
+
+	// 	// Grid for the next generation
+	// 	Grid nextGeneration;
+
+	// 	// Populate the cells in the next generation
+	// 	Calculate(currentGeneration, nextGeneration);
+
+	// 	// Update to the next generation
+	// 	currentGeneration.Update(nextGeneration);
+	// }
+
+	// // Move cursor to bottom of screen
+	// std::cout << "\x1b[" << 0 << ";" << ROW_MAX - 1 << "H";
+
+	//Terminate GLFW
+	glfwTerminate();
+	return 0;
 	// Restore console on Windows
-	RestoreConsole();
+	// RestoreConsole();
 }
